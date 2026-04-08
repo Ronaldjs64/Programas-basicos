8ca08811f3502b3712ee1f05b9f71073cddab15a
Ronald_Sebastian <ackerman.x1124@gamil.com>
2026-04-08 08:46:20 -0500

    Modificacion

--- src/Hello.java
+++ src/Hello.java
@@ -2,17 +2,4 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
-}    1  where java
-    2  nano ~/.bashrc
-    3  nano ~/.bashrc export PS1="ronald@\h \w $"
-    4  nano ~/.bashrc
-    5  source ~/.bashrc
-    6  java -version
-    7  pwd
-    8  javac Hello.java
-    9  javac Hello.java
-   10  javac Hello.java
-   11  java Hello
-   12  history 
-   13  history >> Hello.java
-#Bryan corgi and pug
+}   
